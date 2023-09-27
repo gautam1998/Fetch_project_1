2c5420f6eb7ad83131c95f9931de2d8f36bf0e4d
Iruvanti Gautam Srinidhi <gautams818@gmail.com>
2023-09-27 14:42:15 -0500

    Removing Tensorflow Dependencies

--- fetch_embedding_prep.py
+++ fetch_embedding_prep.py
@@ -1,58 +1,51 @@
+import torch
 import pandas as pd
 from datasets import Dataset
-from transformers import AutoTokenizer, TFAutoModel
+from transformers import AutoTokenizer, AutoModel
 
 def concat_text(text):
-
-  return {
-      "text":str(text["RETAILER"])
+    return {
+        "text": str(text["RETAILER"])
         + " \n "
         + str(text["BRAND"])
         + " \n "
         + str(text["PRODUCT_CATEGORY"])
         + " \n "
         + str(text["IS_CHILD_CATEGORY_TO"])
-  }
-
+    }
 def cls_pooling(model_output):
     return model_output.last_hidden_state[:, 0]
 
 def get_embeddings(text_list, model, tokenizer):
-
     encoded_input = tokenizer(
-        text_list, padding=True, truncation=True, return_tensors="tf"
+        text_list, padding=True, truncation=True, return_tensors="pt"
     )
-    encoded_input = {k: v for k, v in encoded_input.items()}
     model_output = model(**encoded_input)
     return cls_pooling(model_output)
 
 def generate_embeddings(offers_dataset):
-
     model_ckpt = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
     tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-    model = TFAutoModel.from_pretrained(model_ckpt, from_pt=True)
+    model = AutoModel.from_pretrained(model_ckpt)
     embeddings_dataset = offers_dataset.map(
-    lambda x: {"embeddings": get_embeddings(x["text"],model,tokenizer).numpy()[0]}
+        lambda x: {"embeddings": get_embeddings(x["text"], model, tokenizer).detach().numpy()[0]}
     )
     embeddings_dataset.save_to_disk("data/processed/embeddings.hf")
     return embeddings_dataset
 
 def generate_dataset(offer_data):
-
     offers_dataset = Dataset.from_pandas(offer_data)
     offers_dataset = offers_dataset.map(concat_text)
     return offers_dataset
 
 def combined_dataset(brand_data, category_data, offer_data):
-
-    df_offer_and_brands = df_offer_retailer.merge(df_brand_category,on='BRAND',how='inner')
-    df_offer_brands_categories = df_offer_and_brands.merge(df_categories,left_on='BRAND_BELONGS_TO_CATEGORY',right_on='PRODUCT_CATEGORY',how='inner')
-    df_offer_brands_categories = df_offer_brands_categories[['OFFER','RETAILER','BRAND','PRODUCT_CATEGORY','IS_CHILD_CATEGORY_TO']]
+    df_offer_and_brands = df_offer_retailer.merge(df_brand_category, on='BRAND', how='inner')
+    df_offer_brands_categories = df_offer_and_brands.merge(df_categories, left_on='BRAND_BELONGS_TO_CATEGORY', right_on='PRODUCT_CATEGORY', how='inner')
+    df_offer_brands_categories = df_offer_brands_categories[['OFFER', 'RETAILER', 'BRAND', 'PRODUCT_CATEGORY', 'IS_CHILD_CATEGORY_TO']]
     df_offer_brands_categories.to_csv('combined_dataset.csv')
     return df_offer_brands_categories
 
 if __name__ == "__main__":
-    
     df_brand_category = pd.read_csv('./data/raw/brand_category.csv')
     df_categories = pd.read_csv('./data/raw/categories.csv')
     df_offer_retailer = pd.read_csv('./data/raw/offer_retailer.csv')
--- your_app.py
+++ your_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
-from datasets import Dataset
-from transformers import AutoTokenizer, TFAutoModel
-from datasets import load_from_disk
+from datasets import Dataset, load_from_disk
+from transformers import AutoTokenizer, AutoModel
 import streamlit as st
 from fetch_embedding_prep import get_embeddings
 
@@ -9,26 +8,24 @@ def perform_search(query, dataset):
 
     model_ckpt = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
     tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-    model = TFAutoModel.from_pretrained(model_ckpt, from_pt=True)
+    model = AutoModel.from_pretrained(model_ckpt)
 
-    question_embedding = get_embeddings([query], model, tokenizer).numpy()
+    question_embedding = get_embeddings([query], model, tokenizer).detach().numpy()
     dataset.add_faiss_index(column="embeddings")
     scores, samples = dataset.get_nearest_examples(
-    "embeddings", question_embedding, k=5
+        "embeddings", question_embedding, k=5
     )
 
-    return scores,samples
+    return scores, samples
 
 def format_results(scores, samples):
-
     samples_df = pd.DataFrame.from_dict(samples)
     samples_df["scores"] = scores
     samples_df.sort_values("scores", ascending=False, inplace=True)
-    samples_df.drop('embeddings',axis=1,inplace=True)
+    samples_df.drop('embeddings', axis=1, inplace=True)
     desired_column_order = ['OFFER', 'scores', 'RETAILER', 'BRAND', 'PRODUCT_CATEGORY', 'IS_CHILD_CATEGORY_TO']
     samples_df = samples_df[desired_column_order]
     return samples_df
-
 
 def main():
     st.title("Search Engine")
@@ -36,12 +33,12 @@ def main():
     # Load the embeddings dataset
     dataset = load_from_disk("data/processed/embeddings.hf")
 
-    # Text input for user to enter a query
+    # Text input for the user to enter a query
     user_query = st.text_input("Enter your query:")
 
     if st.button("Search"):
         if user_query:
-            scores,samples = perform_search(user_query, dataset)
+            scores, samples = perform_search(user_query, dataset)
             result_df = format_results(scores, samples)
             st.write("Search Results:")
             st.write("Row Data:")
@@ -50,10 +47,5 @@ def main():
         else:
             st.warning("Please enter a query.")
 
-    
-
 if __name__ == "__main__":
     main()
-
-
-
